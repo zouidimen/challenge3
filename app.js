86d8f326a81e1a3b4bea58ae5c08faad5a787f46
zouidimen <zouidimen4@gmail.com>
2025-09-08 21:59:05 +0100

    challenge3

--- app.js
+++ app.js
@@ -77,16 +77,16 @@
 // const tip = (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.2;
 // console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
 
-const calcTip = bill => (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.2;
-console.log(calcTip(100)); 
+// const calcTip = bill => (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.2;
+// console.log(calcTip(100)); 
 
-const bills = [125, 555, 44];
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+// const bills = [125, 555, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 
-console.log(bills);  // [125, 555, 44]
-console.log(tips);   // [18.75, 111, 8.8]
-console.log(total);  // [143.75, 666, 52.8]
+// console.log(bills);  // [125, 555, 44]
+// console.log(tips);   // [18.75, 111, 8.8]
+// console.log(total);  // [143.75, 666, 52.8]
 
 
 
@@ -120,3 +120,34 @@ console.log(total);  // [143.75, 666, 52.8]
 // const avgKoalas2 = calcAverage(23, 34, 27);
 // checkWinner(avgDolphins2, avgKoalas2);
 
+//challenge4:::
+
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function() {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  }
+};
+
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function() {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  }
+};
+
+mark.calcBMI();
+john.calcBMI();
+
+// 3. Comparaison et affichage du résultat
+if (mark.bmi > john.bmi) {
+  console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})!`);
+} else {
+  console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})!`);
+}
